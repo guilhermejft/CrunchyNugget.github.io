6a5c377d8fe18b32f06b75969a9a99166f27d1eb
guilhermejft <127132343+guilhermejft@users.noreply.github.com>
2023-08-28 17:30:10 +0100

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -329,3 +329,14 @@ function detectCollision(a, b) {
         a.y + a.height > b.y
     );
 }
+
+const winGifContainer = document.getElementById("winGifContainer");
+const winGif = document.getElementById("winGif");
+
+winGifContainer.addEventListener("touchstart", handleWinGifTouch);
+winGif.addEventListener("touchstart", handleWinGifTouch);
+
+function handleWinGifTouch(e) {
+    // Call the moveBird function to make the bird jump
+    moveBird(e);
+}
