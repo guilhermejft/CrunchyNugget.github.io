2560fef93453a1b2baa0734671f154718dfc9da9
guilhermejft <127132343+guilhermejft@users.noreply.github.com>
2023-08-30 15:38:24 +0100

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -105,14 +105,21 @@ window.onload = function () {
 
 function startGame() {
     if (!gameStarted) {
-        gameStarted = true;
-        pipesInterval = setInterval(placePipes, initialInterval);
-        playJumpSound();
-        if (gameOver) {
-            resetGame();
-        }
+      gameStarted = true;
+      pipesInterval = setInterval(placePipes, initialInterval);
+      playJumpSound();
+      if (gameOver) {
+        resetGame();
+      }
+      hideWinGif(); // Hide the win GIF when the game starts
     }
-}
+  }
+
+  function hideWinGif() {
+    const winGif = document.getElementById("winGif");
+    winGif.style.display = "none";
+    winGif.style.top = "100%"; // Move the GIF below the board
+  }  
 
 function update() {
     requestAnimationFrame(update);
@@ -319,9 +326,10 @@ function resetGame() {
 }
 
 function displayWinGif() {
-  const winGif = document.getElementById("winGif");
-  winGif.style.display = "block";
-}
+    const winGif = document.getElementById("winGif");
+    winGif.style.top = "0"; // Center the GIF vertically
+    winGif.style.display = "block";
+  }  
 
 function detectCollision(a, b) {
     return (
